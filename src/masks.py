388a8ae4e746d73263444e9445d50c74f72eab86
Vadim Kochergin <vadim-kochergin@mail.ru>
2025-01-21 02:04:16 +0400

    update masks.py and test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,4 @@
-def get_mask_card_number(card_number: int = 0) -> str:
+def get_mask_card_number(card_number: int = None) -> str:
     """
     принимает на вход номер карты и возвращает ее маску
     в формате: XXX XX** **** XXXX, где X — это цифра номера
@@ -6,26 +6,37 @@ def get_mask_card_number(card_number: int = 0) -> str:
     7000 79** **** 6361  - выход функции
     """
 
-    card_number_s = str(card_number)
-
-    if card_number_s == '0':
+    if card_number == None:
         raise ValueError('Номер карты не может отсутствовать')
+
+    card_number_s = str(card_number)
 
     if len(card_number_s) != 16:
         raise ValueError('Неверная длина номера карты')
 
-    if any(digit.isalpha() for digit in card_number_s):
+    if any(not digit.isdigit() for digit in card_number_s):
         raise ValueError('Неверный формат номера карты')
 
     return f"{card_number_s[:4]} {card_number_s[4:6]}** **** {card_number_s[-4:]}"
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: int = None) -> str:
     """
     принимает на вход номер счёта и возвращает его маску
     пример:
     73654108430135874305  # входной аргумент
     **4305  # выход функции
     """
+
+    if account_number == None:
+        raise ValueError('Номер счёта не может отсутствовать')
+
     account_number_s = str(account_number)
+
+    if len(account_number_s) != 20:
+        raise ValueError('Неверная длина номера карты')
+
+    if any(not digit.isdigit() for digit in account_number_s):
+        raise ValueError('Неверный формат номера карты')
+
     return f"**{account_number_s[-4:]}"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -13,8 +13,23 @@ def test_get_mask_empty_card_number():
         get_mask_card_number()
 
 
-@pytest.mark.parametrize('card_number', [111,
-                                         111111111111111111111111111111111111111111111])
+@pytest.mark.parametrize('card_number', [111, 111111111111111111111111111111111111111111111])
 def test_get_mask_card_number_wrong_length(card_number):
     with pytest.raises(ValueError):
         get_mask_card_number(card_number)
+
+
+@pytest.mark.parametrize('account_number, expected', [(73654108430135874305, '**4305'),
+                                                      (70008122896074597512, '**7512')])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
+
+def test_get_mask_empty_account():
+    with pytest.raises(ValueError):
+        get_mask_account()
+
+
+@pytest.mark.parametrize('account_number', [111, 111111111111111111111111111111111111111111111])
+def test_get_mask_account_wrong_length(account_number):
+    with pytest.raises(ValueError):
+        get_mask_card_number(account_number)
