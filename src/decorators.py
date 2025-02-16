d8504aa8ba6e5f08197e4eaaa0fac965e8e490d2
vadimko4 <vad.informat@gmail.com>
2025-02-16 20:44:13 +0400

    update readme, apply linters

--- src/decorators.py
+++ src/decorators.py
@@ -4,7 +4,8 @@ from typing import Any
 def log(filename: str = '') -> Any:
     """
     Декоратор автоматически логирует результаты выполнения функции, или возникшие ошибки.
-    Принимает необязательный аргумент filename, который определяет, куда будут записываться логи (в файл или в консоль):
+    Принимает необязательный аргумент filename, который определяет,
+    куда будут записываться логи (в файл или в консоль):
     если filename задан, логи записываются в указанный файл.
     если не задан, логи выводятся в консоль.
     Логирование включает:
@@ -13,33 +14,22 @@ def log(filename: str = '') -> Any:
     """
     def decorator(func):
         def wraper(*args, **kwargs):
-            #start_time = time()
             if filename:
                 with open(filename, "a", encoding='utf-8') as file:
-                    #file.write(f"{func.__name__} {start_time = }\n")
                     try:
                         result = func(*args, **kwargs)
-                        #end_time = time()
                         file.write(f"{func.__name__} ok\n")
-                        #file.write(f"{func.__name__} {end_time = }\n\n")
                         return result
                     except Exception as e:
-                        #end_time = time()
                         file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
-                        #file.write(f"{func.__name__} {end_time = }\n\n")
                         raise Exception(e)
             else:
-                #print(f"\n{func.__name__} {start_time = }")
                 try:
                     result = func(*args, **kwargs)
-                    #end_time = time()
                     print(f"{func.__name__} ok")
-                    #print(f"{func.__name__} {end_time = }\n")
                     return result
                 except Exception as e:
-                    #end_time = time()
                     print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
-                    #print(f"{func.__name__} {end_time = }\n")
                     raise Exception(e)
         return wraper
     return decorator
