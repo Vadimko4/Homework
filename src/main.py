0f57858f8cfacd8041bb9154f7753aebeaf89a1a
vadimko4 <vad.informat@gmail.com>
2025-03-18 00:16:40 +0400

    update main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+from src.utils import  get_fin_transactions_from_json, PATH_TO_JSON_FILE
+from src.tables import get_transactions_list_from_csv, get_transactions_list_from_xlsx, \
+    PATH_TO_TRANSACTIONS_XLSX_FILE, PATH_TO_TRANSACTIONS_CSV_FILE
+from src.processing import filter_by_state, sort_by_date
+from src.generators import filter_by_currency
+
+
 def foolproof_user_input(valid_values: list[str]) -> str:
     user_answer = ''
     while user_answer not in valid_values:
@@ -16,21 +23,34 @@ print("""Программа: Привет! Добро пожаловать в п
 user_answer = foolproof_user_input(['1', '2', '3'])
 user_cases = {'1': 'JSON-файл', '2': 'CSV-файл', '3': 'XLSX-файл'}
 print(f"\nПрограмма: для обработки выбран {user_cases[user_answer]}")
+if user_answer == '1':
+    transactions_list = get_fin_transactions_from_json(PATH_TO_JSON_FILE)
+elif user_answer == '2':
+    transactions_list = get_transactions_list_from_csv(PATH_TO_TRANSACTIONS_CSV_FILE)
+else:
+    transactions_list = get_transactions_list_from_xlsx(PATH_TO_TRANSACTIONS_XLSX_FILE)
 
 print("""\nПрограмма: Введите статус, по которому необходимо выполнить фильтрацию. 
 Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING (1/2/3)""")
 user_answer = foolproof_user_input(['1', '2', '3'])
 user_cases = {'1': 'EXECUTED', '2': 'CANCELED', '3': 'PENDING'}
-print(f'\nПрограмма: Операции отфильтрованы по статусу "{user_answer}"')
+transactions_list = filter_by_state(transactions_list, user_cases[user_answer])
+print(f'\nПрограмма: Операции отфильтрованы по статусу "{user_cases[user_answer]}"')
 
 print("\nПрограмма: Отсортировать операции по дате? (Да/Нет)")
 user_answer = foolproof_user_input(['ДА', 'НЕТ'])
-
-print("\nПрограмма: Отсортировать по возрастанию или по убыванию? (1/2)")
-user_answer = foolproof_user_input(['1', '2'])
+if user_answer == 'ДА':
+    print("\nПрограмма: Отсортировать по возрастанию или по убыванию? (1/2)")
+    user_answer = foolproof_user_input(['1', '2'])
+    if user_answer == '1':
+        transactions_list = sort_by_date(transactions_list, False)
+    else:
+        transactions_list = sort_by_date(transactions_list)
 
 print("\nПрограмма: Выводить только рублевые тразакции? (Да/Нет)")
 user_answer = foolproof_user_input(['ДА', 'НЕТ'])
+if user_answer == 'ДА':
+    transactions_list = filter_by_currency(transactions_list, 'РУБ')
 
 print("\nПрограмма: Отфильтровать список транзакций по определенному слову в описании? (Да/Нет)")
 user_answer = foolproof_user_input(['ДА', 'НЕТ'])
